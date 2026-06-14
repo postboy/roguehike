e62db9e2766bc6f8ff7765c1686fca01e562c4ad
Ivan Zuboff <postboy@users.noreply.github.com>
2026-06-14 11:40:51 +0300

    bump clojure version to latest
    it makes sense for an application not a library

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "A minimalistic roguelike hiking game. Go climb a mountain here!"
   :url "https://github.com/postboy/roguehike"
   :license {:name "MIT/X11"}
-  :dependencies [[org.clojure/clojure "1.11.0"]
+  :dependencies [[org.clojure/clojure "1.12.5"]
                  [clojure-lanterna "0.9.7"]]
   :profiles {:uberjar {:aot :all}}
   :main roguehike.core)
