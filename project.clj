dae85d20c8954dc852f0f2320468a72e7a611eb2
Ivan Zuboff <postboy@users.noreply.github.com>
2026-07-30 06:05:00 +0300

    version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject roguehike "0.3"
+(defproject roguehike "0.4"
   :description "A minimalistic roguelike hiking game. Go climb a mountain here!"
   :url "https://github.com/postboy/roguehike"
   :license {:name "MIT/X11"}
