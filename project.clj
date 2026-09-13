7045bd38075869dbff3235555e5ab12035f84100
Ivan Zuboff <postboy@users.noreply.github.com>
2026-09-13 18:21:36 +0300

    bump Clojure version

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "A minimalistic roguelike hiking game. Go climb a mountain here!"
   :url "https://github.com/postboy/roguehike"
   :license {:name "MIT/X11"}
-  :dependencies [[org.clojure/clojure "1.12.5"]
+  :dependencies [[org.clojure/clojure "1.12.6"]
                  [clojure-lanterna "0.9.7"]
                  [roul "0.2.0"]]
   :profiles {:uberjar {:aot :all}}
